1f20b033c668f0867e1045a7451c771e611c37f4
pedropcao <pedro.catunta@cooperativailo.pe>
2025-06-30 23:36:46 -0500

    primer commit

--- pycancer/settings.py
+++ pycancer/settings.py
@@ -121,3 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['localhost','gpycancer-production.up.railway.app', '127.0.0.1'] # LA DIRECCION DE NUESTRA APP
+
